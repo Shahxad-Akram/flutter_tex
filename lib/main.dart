d698347757d1749ac0341c51fd147b80219647e1
shahzad <shahzadakram67@gmail.com>
2019-02-17 09:38:32 +0500

    full html support from dart.

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,41 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String teX = Uri.encodeComponent(r"""
   
+  
+  
+  <style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+  transition: 0.3s;
+  width: 40%;
+}
 
+.card:hover {
+  box-shadow: 0 8px 16px 0 rgba(0,0,0,0.2);
+}
+
+.container {
+  padding: 2px 16px;
+}
+</style>
+
+<div class="card">
+  <div class="container">
+
+  <p>
+\begin{align}
+\dot{x} & = \sigma(y-x) \\
+\dot{y} & = \rho x - y - xz \\
+\dot{z} & = -\beta z + xy
+\end{align}
+</p>
+
+  </div>
+</div>
+  
+  
+  
+  
   <p>    
   
   A simple Example to render \( \rm\\TeX \) in flutter<br>
