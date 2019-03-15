4db69a0dfe15014c5071bc9d8b3ae672f6b31968
shahzad <shahzadakram67@gmail.com>
2019-03-15 17:29:13 +0500

    Converted into Package

--- README.md
+++ README.md
@@ -1,5 +1,27 @@
 # Flutter TeX
 
-A Flutter Application demonstrates how to render **Mathematics, Physics** and **Chemistry Equations** based on **LaTeX** in Flutter with full html support using [webview_flutter](https://pub.dartlang.org/packages/webview_flutter) plugin . This is based on [mini-mathjax](https://github.com/electricbookworks/mini-mathjax) a simplified version of [MathJax](https://github.com/mathjax/MathJax/) a JavaScript library.
+A Flutter Plugin helps to render **Mathematics, Physics** and **Chemistry Equations** based on **LaTeX** with full html support using [webview_flutter](https://pub.dartlang.org/packages/webview_flutter) plugin . This is based on [mini-mathjax](https://github.com/electricbookworks/mini-mathjax) a simplified version of [MathJax](https://github.com/mathjax/MathJax/) a JavaScript library.
 
 
+
+#### Use this package as a library
+
+**1:** Add this to your package's pubspec.yaml file:
+
+    dependencies:
+        flutter_tex: ^0.0.13
+
+
+
+**2:** You can install packages from the command line:
+
+    $ flutter packages get
+
+Alternatively, your editor might support flutter packages get. Check the docs for your editor to learn more.
+
+
+**3:** Now in your Dart code, you can use:
+
+
+    import 'package:flutter_tex/flutter_tex.dart';
+ 
---
+++ lib/flutter_tex.dart
@@ -0,0 +1,117 @@
+library flutter_tex;
+import 'package:flutter/widgets.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:io';
+import 'dart:async';
+import 'package:flutter/services.dart';
+import 'package:mime/mime.dart';
+
+
+typedef void LaTeXViewCreatedCallback();
+
+class TeXView extends StatefulWidget {
+  final String teXHTML;
+  final Key key;
+  final LaTeXViewCreatedCallback onTeXViewCreated;
+
+  TeXView({this.key, this.teXHTML, this.onTeXViewCreated});
+
+  @override
+  _LaTeXViewState createState() => _LaTeXViewState();
+}
+
+class _LaTeXViewState extends State<TeXView> {
+  _Server server = _Server();
+
+  @override
+  Widget build(BuildContext context) {
+    server.start();
+    return WebView(
+      key: widget.key,
+      initialUrl:
+      "http://localhost:8080/packages/flutter_tex/MathJax/index.html?data=${widget.teXHTML}",
+      javascriptMode: JavascriptMode.unrestricted
+    );
+  }
+
+  @override
+  void dispose() {
+    server.close();
+    super.dispose();
+  }
+}
+
+
+class _Server {
+
+  // class from inAppBrowser
+
+
+  HttpServer _server;
+
+  int _port = 8080;
+
+  _Server({int port = 8080}) {
+    this._port = port;
+  }
+
+  Future<void> start() async {
+    if (this._server != null) {
+      throw Exception('Server already started on http://localhost:$_port');
+    }
+
+    var completer = new Completer();
+
+    runZoned(() {
+      HttpServer.bind('127.0.0.1', _port).then((server) {
+        print('Server running on http://localhost:' + _port.toString());
+
+        this._server = server;
+
+        server.listen((HttpRequest request) async {
+          var body = List<int>();
+          var path = request.requestedUri.path;
+          path = (path.startsWith('/')) ? path.substring(1) : path;
+          path += (path.endsWith('/')) ? 'index.html' : '';
+
+          try {
+            body = (await rootBundle.load(path))
+                .buffer.asUint8List();
+          } catch (e) {
+            print(e.toString());
+            request.response.close();
+            return;
+          }
+
+          var contentType = ['text', 'html'];
+          if (!request.requestedUri.path.endsWith('/') &&
+              request.requestedUri.pathSegments.isNotEmpty) {
+            var mimeType = lookupMimeType(
+                request.requestedUri.path, headerBytes: body);
+            if (mimeType != null) {
+              contentType = mimeType.split('/');
+            }
+          }
+
+          request.response.headers.contentType =
+          new ContentType(contentType[0], contentType[1], charset: 'utf-8');
+          request.response.add(body);
+          request.response.close();
+        });
+
+        completer.complete();
+      });
+    }, onError: (e, stackTrace) => print('Error: $e $stackTrace'));
+
+    return completer.future;
+  }
+
+  ///Closes the server.
+  Future<void> close() async {
+    if (this._server != null) {
+      await this._server.close(force: true);
+      print('Server running on http://localhost:$_port closed');
+      this._server = null;
+    }
+  }
+}
